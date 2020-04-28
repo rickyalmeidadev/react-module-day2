080302e8077c295ac7b5f167e0eef813c65a8082
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-04-27 22:46:11 -0300

    add ReactPlayer component and test some props on it

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import './App.css'
 import Header from './components/Header.js'
 import User from './components/User.js'
+import ReactPlayer from "react-player"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <User firstName="Ricky" />
         <User firstName="Carlao" />
+        
+        {/* Vimeo video */}
+        <ReactPlayer url="https://vimeo.com/channels/top/22439234" />
+
+        {/* YouTube Video */}
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=kJQP7kiw5Fk"
+          playing
+          controls
+          muted
+        />
       </div>
     )
   }
